2b3b30eddc7536c11309625c43703b0b2a834f72
jjomena <joaquinmena.84@gmail.com>
2019-09-18 22:27:02 -0600

    Inicialización del proyecto

---
+++ AdministracionTiempos/AdministracionTiempos/Tiempos.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace AdministracionTiempos
+{
+    partial class Tiempos
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxNum = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtApuesta = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.comboBoxEstado = new System.Windows.Forms.ComboBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TotalTiempos = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnAct = new System.Windows.Forms.Button();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.comboBoxApostador = new System.Windows.Forms.ComboBox();
+            this.dataGridTiempos = new System.Windows.Forms.DataGridView();
+            this.Número = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Apuesta = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTiempos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.textBoxNum);
+            this.groupBox3.Controls.Add(this.btnIngresar);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Controls.Add(this.txtApuesta);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.txtNum);
+            this.groupBox3.Location = new System.Drawing.Point(26, 28);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(391, 237);
+            this.groupBox3.TabIndex = 25;
+            this.groupBox3.TabStop = false;
+            // 
+            // textBoxNum
+            // 
+            this.textBoxNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNum.Location = new System.Drawing.Point(219, 55);
+            this.textBoxNum.Name = "textBoxNum";
+            this.textBoxNum.Size = new System.Drawing.Size(109, 26);
+            this.textBoxNum.TabIndex = 23;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(154, 176);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(110, 44);
+            this.btnIngresar.TabIndex = 22;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            this.btnIngresar.Click += new System.EventHandler(this.BtnIngresar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(65, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 20);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Número : ";
+            // 
+            // txtApuesta
+            // 
+            this.txtApuesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtApuesta.Location = new System.Drawing.Point(219, 116);
+            this.txtApuesta.Name = "txtApuesta";
+            this.txtApuesta.Size = new System.Drawing.Size(133, 26);
+            this.txtApuesta.TabIndex = 21;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(60, 119);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(142, 20);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Ingrese Apuesta";
+            // 
+            // txtNum
+            // 
+            this.txtNum.AutoSize = true;
+            this.txtNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum.Location = new System.Drawing.Point(183, 55);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(0, 20);
+            this.txtNum.TabIndex = 20;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dateTimePicker2);
+            this.groupBox2.Controls.Add(this.comboBoxEstado);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.TotalTiempos);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.btnGuardar);
+            this.groupBox2.Controls.Add(this.btnAct);
+            this.groupBox2.Controls.Add(this.btnAgregar);
+            this.groupBox2.Controls.Add(this.comboBoxApostador);
+            this.groupBox2.Controls.Add(this.dataGridTiempos);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(482, 4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(471, 443);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "INFORMACIÓN GENERAL";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(132, 82);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 32;
+            // 
+            // comboBoxEstado
+            // 
+            this.comboBoxEstado.FormattingEnabled = true;
+            this.comboBoxEstado.Items.AddRange(new object[] {
+            "Cancelado",
+            "Pendiente"});
+            this.comboBoxEstado.Location = new System.Drawing.Point(107, 320);
+            this.comboBoxEstado.Name = "comboBoxEstado";
+            this.comboBoxEstado.Size = new System.Drawing.Size(166, 21);
+            this.comboBoxEstado.TabIndex = 31;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(40, 320);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 30;
+            this.label7.Text = "Estado :";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Enabled = false;
+            this.dateTimePicker1.Location = new System.Drawing.Point(132, 24);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 30;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(202, 275);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 20);
+            this.label8.TabIndex = 29;
+            this.label8.Text = "Total : ";
+            // 
+            // TotalTiempos
+            // 
+            this.TotalTiempos.AutoSize = true;
+            this.TotalTiempos.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalTiempos.Location = new System.Drawing.Point(260, 275);
+            this.TotalTiempos.Name = "TotalTiempos";
+            this.TotalTiempos.Size = new System.Drawing.Size(18, 20);
+            this.TotalTiempos.TabIndex = 28;
+            this.TotalTiempos.Text = "0";
+            this.TotalTiempos.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Fecha Actual : ";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(297, 414);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 24;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnAct
+            // 
+            this.btnAct.Location = new System.Drawing.Point(89, 393);
+            this.btnAct.Name = "btnAct";
+            this.btnAct.Size = new System.Drawing.Size(75, 23);
+            this.btnAct.TabIndex = 18;
+            this.btnAct.Text = "Actualizar";
+            this.btnAct.UseVisualStyleBackColor = true;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(183, 393);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 17;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxApostador
+            // 
+            this.comboBoxApostador.FormattingEnabled = true;
+            this.comboBoxApostador.Location = new System.Drawing.Point(132, 347);
+            this.comboBoxApostador.Name = "comboBoxApostador";
+            this.comboBoxApostador.Size = new System.Drawing.Size(166, 21);
+            this.comboBoxApostador.TabIndex = 16;
+            this.comboBoxApostador.Text = "Seleccione el Nombre";
+            // 
+            // dataGridTiempos
+            // 
+            this.dataGridTiempos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridTiempos.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Número,
+            this.Apuesta});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.Green;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridTiempos.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridTiempos.Location = new System.Drawing.Point(51, 122);
+            this.dataGridTiempos.Name = "dataGridTiempos";
+            this.dataGridTiempos.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridTiempos.Size = new System.Drawing.Size(257, 150);
+            this.dataGridTiempos.TabIndex = 11;
+            // 
+            // Número
+            // 
+            this.Número.HeaderText = "Número";
+            this.Número.Name = "Número";
+            // 
+            // Apuesta
+            // 
+            this.Apuesta.HeaderText = "Apuesta";
+            this.Apuesta.Name = "Apuesta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(41, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Nombre Cliente :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Fecha del Sorteo :";
+            // 
+            // Tiempos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1063, 484);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Tiempos";
+            this.Text = "TIEMPOS";
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridTiempos)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxNum;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtApuesta;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label txtNum;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.ComboBox comboBoxEstado;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label TotalTiempos;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnAct;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.ComboBox comboBoxApostador;
+        private System.Windows.Forms.DataGridView dataGridTiempos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Número;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Apuesta;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AdministracionTiempos/AdministracionTiempos/Tiempos.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdministracionTiempos
+{
+    public partial class Tiempos : Form
+    {
+        public Tiempos()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIngresar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String Num = textBoxNum.Text;
+                int pruebaNum = int.Parse(Num);
+                if ((pruebaNum > 0) && (pruebaNum < 99))
+                {
+                    int Apostar = int.Parse(txtApuesta.Text);
+                    //dataGridTiempos.Rows.Add(Num, Apostar);
+                    AgregarTiempos(Num, Apostar);
+                    int total = int.Parse(TotalTiempos.Text);
+                    total = total + Apostar;
+                    TotalTiempos.Text = total.ToString();
+                    textBoxNum.Text = "";
+                    txtApuesta.Text = "";
+                }
+                else
+                {
+                    MessageBox.Show("Los valores válidos se encuentran entre el rango de [00...99]", "Vefifique los datos ingresados", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    txtApuesta.Text = "";
+                    textBoxNum.Text = "";
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Verifique los datos ingresados ", "Datos Inválidos ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtApuesta.Text = "";
+                textBoxNum.Text = "";
+            }
+        }
+
+        private void AgregarTiempos(String Num, int Apuesta)
+        {
+            string Numero;
+            int CantidadApuesta;
+            int Control = 0;
+            if (dataGridTiempos.Rows.Count > 1)
+            {
+                for (int counter = 0; counter < (dataGridTiempos.Rows.Count - 1); counter++)
+                {
+                    Numero = dataGridTiempos.Rows[counter].Cells[0].Value.ToString();
+                    if (Numero == Num)
+                    {
+                        CantidadApuesta = int.Parse(dataGridTiempos.Rows[counter].Cells[1].Value.ToString());
+                        CantidadApuesta += Apuesta;
+                        dataGridTiempos.Rows[counter].Cells[1].Value = CantidadApuesta;
+                        Control = 1;
+                        break;
+                    }
+                }
+                if (Control == 0)
+                {
+                    this.dataGridTiempos.Rows.Add(Num, Apuesta);
+                }
+            }
+            else
+            {
+                this.dataGridTiempos.Rows.Add(Num, Apuesta);
+            }
+        }
+
+    }
+}
